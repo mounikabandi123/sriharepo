77f6276a691d59332609364c5b5e7684b9a2879d
mounikabandi123 <mounikabandi50@gmail.com>
2021-05-12 10:24:39 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,14 @@
+class demo
+{
+ public static void main(...)
+ {
+ s.o.print ("good day")
+ s.o.print ("good day")
+ s.o.print ("good day")
+ s.o.print ("good day")
+ s.o.print ("good day")
+ s.o.print ("good day")
+ s.o.print ("good day") 
+ s.o.print ("good day")
+ }
+}
